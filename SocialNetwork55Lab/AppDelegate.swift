039e0e46d119c457885dde4105f1c508bc093210
Huallyd <huallyd@gmail.com>
2017-03-07 13:13:40 -0300

    other push

--- SocialNetwork55Lab/AppDelegate.swift
+++ SocialNetwork55Lab/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         Fabric.with([Crashlytics.self])
         
-        //
+        
         if DefaultsHelper.sharedInstance.email == ""  || DefaultsHelper.sharedInstance.email == nil {
             let storyboard = UIStoryboard(name: "Authentication", bundle: nil)
             let vcToShow = storyboard.instantiateInitialViewController()
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.window?.rootViewController = vcToShow
         }
       
-        // Override point for customization after application launch.
+    
         return true
     }
 
