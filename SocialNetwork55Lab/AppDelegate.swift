a55924f68dca65f65ce5c64b617959c0faaf10f7
Huallyd <huallyd@gmail.com>
2017-03-07 13:06:49 -0300

    build information

--- SocialNetwork55Lab/AppDelegate.swift
+++ SocialNetwork55Lab/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         Fabric.with([Crashlytics.self])
         
-        
+        //
         if DefaultsHelper.sharedInstance.email == ""  || DefaultsHelper.sharedInstance.email == nil {
             let storyboard = UIStoryboard(name: "Authentication", bundle: nil)
             let vcToShow = storyboard.instantiateInitialViewController()
