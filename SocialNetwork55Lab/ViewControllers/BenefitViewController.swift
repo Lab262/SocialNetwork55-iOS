e5089e85387cac8b903ead2186bf9c6be77dcf06
luisresende <luisfrvilela@gmail.com>
2017-03-13 23:20:37 -0300

    create benefits table view

--- SocialNetwork55Lab/ViewControllers/BenefitViewController.swift
+++ SocialNetwork55Lab/ViewControllers/BenefitViewController.swift
@@ -10,31 +10,80 @@ import UIKit
 
 class BenefitViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var navigationBarView: IconNavigationBar!
+    
+    func registerNibs() {
+        tableView.registerNibFrom(BenefitHeaderTableViewCell.self)
+        tableView.registerNibFrom(BenefitTitleTableViewCell.self)
+        tableView.registerNibFrom(ActionButtonTableViewCell.self)
+        tableView.registerNibFrom(InformationDescriptionTableViewCell.self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpNavigationBar()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        registerNibs()
+        tableView.estimatedRowHeight = 100.0
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
     
     func setUpNavigationBar() {
         navigationController?.navigationBar.isHidden = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func generateBenefitHeader(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: BenefitHeaderTableViewCell.identifier, for: indexPath)
+        
+        return cell
     }
-    */
-
+    
+    func generateBenefitTitle(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: BenefitTitleTableViewCell.identifier, for: indexPath)
+        
+        return cell
+    }
+    
+    func generateDescriptionInformation(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: InformationDescriptionTableViewCell.identifier, for: indexPath)
+        
+        return cell
+    }
+    
+    func generateActionButton(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ActionButtonTableViewCell.identifier, for: indexPath)
+        
+        return cell
+    }
+    
 }
+
+extension BenefitViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch indexPath.row {
+        case 0:
+            return generateBenefitHeader(tableView, cellForRowAt: indexPath)
+        case 1:
+            return generateBenefitTitle(tableView, cellForRowAt: indexPath)
+        case 2:
+            return generateDescriptionInformation(tableView, cellForRowAt: indexPath)
+        case 3:
+            return generateActionButton(tableView, cellForRowAt: indexPath)
+        default:
+            return UITableViewCell()
+        
+        }
+    }
+}
+
