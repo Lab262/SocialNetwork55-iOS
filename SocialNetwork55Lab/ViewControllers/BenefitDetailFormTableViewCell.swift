8a88ccfe1d150a42fbaadb092d6d0f312d087b7a
luisresende <luisfrvilela@gmail.com>
2017-04-10 23:55:18 -0300

    create BenefitDetailFormTableViewCell

---
+++ SocialNetwork55Lab/ViewControllers/BenefitDetailFormTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BenefitDetailFormTableViewCell.swift
+//  SocialNetwork55Lab
+//
+//  Created by Luís Resende on 10/04/17.
+//  Copyright © 2017 Lab262. All rights reserved.
+//
+
+import UIKit
+
+class BenefitDetailFormTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
